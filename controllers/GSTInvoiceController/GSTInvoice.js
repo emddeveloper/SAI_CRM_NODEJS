1a4ec97db6c2beed055b618a1e12d742eb4f24be
emddeveloper <emd.developer@gmail.com>
2025-06-28 03:41:54 +0530

    SK-commit-puppeteer --no-sandbox added

--- controllers/GSTInvoiceController/GSTInvoice.js
+++ controllers/GSTInvoiceController/GSTInvoice.js
@@ -54,7 +54,10 @@ const createGSTInvoice = async (req, res) => {
     console.log(entryObj);
 
     // Generate PDF with puppeteer
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: "networkidle0" });
     const pdfBuffer = await page.pdf({ format: "A4",printBackground: true });
